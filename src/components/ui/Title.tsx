0b2c08818ee23c51ce1c8402d016b48ddfd98aa0
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-05-19 20:16:05 +0200

    create reusable Title component

---
+++ src/components/ui/Title.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import { StyleSheet, Text } from 'react-native'
+
+export const Title = ({ children }: TitlePropsType) => {
+  return <Text style={styles.title}>{children}</Text>
+}
+
+type TitlePropsType = {
+  children: string
+}
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: '#FFFFFF',
+    padding: 12,
+  },
+})
