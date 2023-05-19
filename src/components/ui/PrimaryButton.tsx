813d06181642d936c1b2583f061425afe59b5d2a
Alex-Fes <aleksei.fesenko86@gmail.com>
2023-05-19 20:15:40 +0200

    create reusable PrimaryButton component

---
+++ src/components/ui/PrimaryButton.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+
+import { Colors } from '../../../constants/colors'
+
+export const PrimaryButton = (props: PrimaryButtonProps): JSX.Element => {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer
+        }
+        onPress={props.onPress}
+        android_ripple={{ color: Colors.primary600 }}
+      >
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </Pressable>
+    </View>
+  )
+}
+
+type PrimaryButtonProps = {
+  children: string
+  onPress: () => void
+}
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 2,
+    overflow: 'hidden',
+  },
+  buttonInnerContainer: {
+    backgroundColor: Colors.primary500,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: '#FFFFFF',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+})
